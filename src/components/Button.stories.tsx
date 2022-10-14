65bc2f64b6ef33db24402c76463821aa99df1ce5
Paulo Pires <prpires66@gmail.com>
2022-10-13 23:44:56 -0300

    initial commit

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj} from '@storybook/react'
+import { Button, ButtonProps } from "./Button"
+
+export default {
+    title: 'Components/Button',
+    component: Button,
+    args: {
+        children: 'Create account',
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: false,
+            }
+        },
+        asChild: {
+            table: {
+                disable: true,
+            }
+        },        
+    },
+} as Meta<ButtonProps>
+
+export const Default: StoryObj<ButtonProps> = {};
