507d76a55d03e58ab8acd2462ccf0d4086f2a8bf
Paulo Pires <prpires66@gmail.com>
2022-10-15 12:51:39 -0300

    projeto final

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { SignIn } from './pages/SignIn';
 import './styles/global.css';
 
 export function App() {
   return (
-    <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
+    <SignIn />
   )
 }
